4fc8f51dd59240405434b01972e07634f0274a32
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-16 05:01:51 +0300

    Create nix package derrivation

--- flake.nix
+++ flake.nix
@@ -7,9 +7,27 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in {
+        packages.default = self.packages.${system}.mklittlefs;
+        packages.mklittlefs = pkgs.stdenv.mkDerivation {
+          name = "mklittlefs";
+          src = fetchGit {
+            url = "https://github.com/ValynTyler/mklittlefs.git";
+            rev = "2887dead1fd78197bed706ed78afb32521b0af6c";
+            submodules = true;
+          };
+          buildInputs = with pkgs; [ gcc git ];
+          buildPhase = ''
+            make dist
+          '';
+          installPhase = ''
+            mkdir -p $out/bin/
+            cp mklittlefs $out/bin/mklittlefs
+          '';
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [ gcc ];
         };
